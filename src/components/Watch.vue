95b326b90359e3005f01a8f61de3d5d7b5853064
Hiroki Tatara <tatara0862@gmail.com>
2021-08-19 15:48:41 +0900

    getDayをgetDateに修正

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -56,63 +56,63 @@ export default {
 </script>
 
 <style scoped>
-  .container {
-    padding: 2%;
-    background-color: #468536;
-  }
+.container {
+  padding: 2%;
+  background-color: #468536;
+}
 
-  .date {
-    margin: 0.5em 0;
-    font-family: "Teko", sans-serif;
-    font-size: 4rem;
-    line-height: 1;
-    color: #fff;
-    text-align: center;
-    letter-spacing: 0.5em;
-  }
+.date {
+  margin: 0.5em 0;
+  font-family: 'Teko', sans-serif;
+  font-size: 4rem;
+  line-height: 1;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.5em;
+}
 
-  .time {
-    display: flex;
-  }
+.time {
+  display: flex;
+}
 
-  .time-item {
-    position: relative;
-    z-index: 1;
-    box-sizing: border-box;
-    display: flex;
-    flex: 1 1;
-    align-items: center;
-    justify-content: center;
-    height: 100px;
-    padding: 0.5em;
-    margin: 3px;
-    font-family: "Roboto Mono", monospace;
-    font-size: 3rem;
-    line-height: 1;
-    color: #fff;
-    background-color: #a23e3e;
-  }
+.time-item {
+  position: relative;
+  z-index: 1;
+  box-sizing: border-box;
+  display: flex;
+  flex: 1 1;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  padding: 0.5em;
+  margin: 3px;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 3rem;
+  line-height: 1;
+  color: #fff;
+  background-color: #a23e3e;
+}
 
-  .time-item::before {
-    position: absolute;
-    right: 5px;
-    bottom: 1px;
-    z-index: 1;
-    font-family: "Teko", sans-serif;
-    font-size: 1.4rem;
-    color: #fdfdfd;
-    letter-spacing: 0.05em;
-  }
+.time-item::before {
+  position: absolute;
+  right: 5px;
+  bottom: 1px;
+  z-index: 1;
+  font-family: 'Teko', sans-serif;
+  font-size: 1.4rem;
+  color: #fdfdfd;
+  letter-spacing: 0.05em;
+}
 
-  .hours::before {
-    content: "Hours";
-  }
+.hours::before {
+  content: 'Hours';
+}
 
-  .minutes::before {
-    content: "Minutes";
-  }
+.minutes::before {
+  content: 'Minutes';
+}
 
-  .seconds::before {
-    content: "Seconds";
-  }
+.seconds::before {
+  content: 'Seconds';
+}
 </style>
